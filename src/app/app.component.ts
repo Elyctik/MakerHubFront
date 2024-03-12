821b7b588fb81aa6d5e4816213d53382028b7b30
Elyctik <depsamuel@hotmail.com>
2024-03-12 16:04:49 +0100

    First commit Front

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import {CalendarEvent, CalendarView} from "angular-calendar";
+import {isSameDay, isSameMonth} from "date-fns";
+import {Subject} from "rxjs";
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,79 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'FrontEnd2';
+
+  viewDate: Date = new Date();
+  view: CalendarView = CalendarView.Week;
+  CalendarView = CalendarView;
+
+  events : CalendarEvent[] =[];
+
+  activeDayIsOpen = false;
+
+  refresh = new Subject<void>()
+  constructor() {
+    const event1 = {
+      title: "Visite de Mr Lorthoir",
+      start: new Date("2024-03-12T10:30"),
+      end: new Date("2024-03-12T17:30"),
+      draggable: true,
+      resizable:{
+        beforeStart: true,
+        afterEnd: true,//On choisit de rentre modifiable le début Et/ou la fin de l'event
+      }
+    }
+      this.events.push(event1);
+
+    const event2 = {
+      title: "Visite de Mr Doe, John Doe",
+      start: new Date("2024-03-14T12:30"),
+      end: new Date("2024-03-14T14:30"),
+      draggable: true,
+      resizable:{
+        beforeStart: true,
+        afterEnd: true,//On choisit de rentre modifiable le début Et/ou la fin de l'event
+      }
+    }
+    this.events.push(event2);
+
+    const event3 = {
+      title: "Visite de Mr Nobody",
+      start: new Date("2024-03-12T12:30"),
+      end: new Date("2024-03-12T13:30"),
+      draggable: true,
+      resizable:{
+        beforeStart: true,
+        afterEnd: true,//On choisit de rentre modifiable le début Et/ou la fin de l'event
+      }
+    }
+    this.events.push(event3);
+  }
+
+
+
+  setView(view: CalendarView){
+    this.view = view
+  }
+
+  dayClicked({date,events}: {date: Date;events: CalendarEvent[]}):void{
+    if (isSameMonth(date, this.viewDate)) {
+      if (
+        (isSameDay(this.viewDate,date) && this.activeDayIsOpen === true) ||
+        events.length === 0
+      ) {
+      this.activeDayIsOpen = false;
+      } else {
+        this.activeDayIsOpen = true;
+      }
+      this.viewDate = date;
+    }
+  }
+
+  eventTimesChanged(event: any){
+    event.event.start = event.newStart;
+    event.event.end = event.newEnd;
+    this.refresh.next();
+  }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,48 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {
+  CalendarDateFormatter,
+  CalendarModule,
+  CalendarNativeDateFormatter,
+  DateAdapter,
+  DateFormatterParams
+} from "angular-calendar";
+import {adapterFactory} from "angular-calendar/date-adapters/date-fns";
+import {registerLocaleData} from "@angular/common";
+import localeFr from'@angular/common/locales/fr';
 
+
+registerLocaleData(localeFr,'fr');
+
+class CustomDateFormatter extends CalendarNativeDateFormatter{
+  public override dayViewHour({date, locale}: DateFormatterParams): string {
+    return new Intl.DateTimeFormat(locale,{hour:'numeric',minute:'numeric'}).format(date);
+  }
+  public override weekViewHour({date, locale}: DateFormatterParams): string {
+    return new Intl.DateTimeFormat(locale,{hour:'numeric',minute:'numeric'}).format(date);
+  }
+}
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateEventComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    CalendarModule.forRoot({
+      provide: DateAdapter,
+      useFactory: adapterFactory,
+    })
   ],
-  providers: [],
+  providers: [
+    {provide: CalendarDateFormatter, useClass: CustomDateFormatter}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
